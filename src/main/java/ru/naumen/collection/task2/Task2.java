eebc96647ac31f6972080594e656a0aa9e653ac4
X-Aspirinka02 <az1610002@gmail.com>
2024-12-02 00:39:39 +0500

    initial commit

--- src/main/java/ru/naumen/collection/task2/Task2.java
+++ src/main/java/ru/naumen/collection/task2/Task2.java
@@ -1,7 +1,5 @@
 package ru.naumen.collection.task2;
-
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Дано:
@@ -36,7 +34,17 @@ public class Task2
      * Возвращает дубликаты пользователей, которые есть в обеих коллекциях
      */
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        // TODO реализовать метод
-        return null;
+        //Используем HashSet для быстрого поиска
+        Set<User> setB = new HashSet<>(collB); // создание O(m) m - длинна B
+
+        List<User> duplicates = new LinkedList<>(); // добавление элемента за O(1)
+        for (User user : collA) { // цикл O(n) n - длинна A
+            if (setB.contains(user)) { //проверяем за O(1) за счет переопределенных методов сравнения объектов
+                duplicates.add(user);
+            }
+        }
+        return duplicates;
     }
+    //средняя сложность O(m+n), где m и n - длины коллекций A и B
 }
+
--- src/main/java/ru/naumen/collection/task2/User.java
+++ src/main/java/ru/naumen/collection/task2/User.java
@@ -1,4 +1,6 @@
 package ru.naumen.collection.task2;
+import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Пользователь
@@ -10,4 +12,33 @@ public class User {
     private String username;
     private String email;
     private byte[] passwordHash;
+
+    /**
+     * Переопределение метода сравнения, т к нам нужны одинаковые поля у объектов, а не одинаковые объекты
+     * @param o передаваемый объект для сравнения
+     * @return true если объект одного класса и поля совпадают
+     */
+    @Override
+    public boolean equals(Object o) {
+        //вдруг не только поля одинаковые, но и объекты
+        if (this == o) return true;
+        // т к стандартный equals должен принимать Object
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        //а тут уже сравниваем поля
+        return username.equals(user.username) && email.equals(user.email) && Arrays.equals(passwordHash, user.passwordHash);
+    }
+    /**
+     * Переопределение hash функции, для переопределения equals
+     * @return hash для объекта
+     */
+    @Override
+    public int hashCode() {
+        //теперь hash функция тоже будет зависеть от полей
+        // так что при сравнении equals, hash тоже будет одинаковый
+        int hash = Objects.hash(username, email);
+        hash = 31 * hash + Arrays.hashCode(passwordHash);
+        return hash;
+    }
+
 }
